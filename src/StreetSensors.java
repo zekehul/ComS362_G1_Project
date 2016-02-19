022ce407361464eee15b2c7d8bf5e6b1fb447a9d
Jon Haut <jhaut@iastate.edu>
2016-02-18 18:41:38 -0600

    System now waits for commands, exit command added

--- src/StreetSensors.java
+++ src/StreetSensors.java
@@ -1,10 +1,32 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StreetSensors {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to the StreetSensor System");
+		boolean exit = false;
+		
+		System.out.print("Welcome to the StreetSensor System\nPlease enter a command:");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(!exit){
+			try {
+				String cmd = br.readLine();
+			
+				switch(cmd){
+					case "exit": exit = true; 
+						break;
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
