b46ce0d25b0f08c9157621aa5833e872cf81ffde
msauber <msauber@iastate.edu>
2016-02-23 14:59:47 -0600

    Added blank methods and two classes

---
+++ src/ServiceRequest.java
@@ -0,0 +1,6 @@
+package src;
+
+public class ServiceRequest {
+	private ArrayList<Sensor> sensors;
+
+}
--- src/Town.java
+++ src/Town.java
@@ -1,5 +1,41 @@
 package src;
 
 public class Town {
-
+	private ArrayList<Sensor> sensors;
+	private ArrayList<Sensor> serviceRequests;
+	
+	public boolean addSensor(int sid, int threshold){
+		
+	}
+	public boolean deleteSensor(int sid){
+		
+	}
+	public boolean updateStrain(int sid)
+	{
+		
+	}
+	public boolean resetSensor(int sid)
+	{
+		
+	}
+	public ArrayList<Sensor> getAllSensors()
+	{
+		
+	}
+	public Sensor getSensor(int sid)
+	{
+		
+	}
+	public ArrayList<Sensor> getCriticalSensors()
+	{
+		
+	}
+	public ArrayList<Sensor> getDeadSensors()
+	{
+		
+	}
+	public boolean createServiceRequest()
+	{
+		
+	}
 }
