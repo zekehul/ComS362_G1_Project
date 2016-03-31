26e81952b8383f096d60e4b0193da0bdb6b4dbb2
zekehul <dezcont@gmail.com>
2016-03-31 17:59:18 -0500

    added comments to TownController and ServiceRequest

--- src/ServiceRequest.java
+++ src/ServiceRequest.java
@@ -5,25 +5,34 @@ import java.util.List;
 
 public class ServiceRequest implements ServiceRequestInterface{
 
+	// service request id number
 	private String srid;
+	// list of sensors in the service request
 	private List<Sensor> sensors;
 	
+	// gets the service request's id number
 	public String getSrid() {
 		return srid;
 	}
 
+	// sets the service request's id number
 	public void setSrid(String srid) {
 		this.srid = srid;
 	}
 
+	// gets the list of sensors in the service request
 	public List<Sensor> getSensors() {
 		return sensors;
 	}
 
+	// sets the list of sensors in the service request
 	public void setSensors(List<Sensor> sensors) {
 		this.sensors = sensors;
 	}
 
+	// adds a sensor s to the list of sensors in the service
+	// request. If the list has not been initialized, it is
+	// initialized here
 	@Override
 	public boolean addSensorToRequest(Sensor s) {
 		if(this.sensors == null){
--- src/TownController.java
+++ src/TownController.java
@@ -6,56 +6,70 @@ public class TownController implements TownControllerInterface{
 
 	private Town town = null;
 	
-	@Override
+	// function to add a sensor to the database
+	@Override	
 	public boolean addSensor(String sid, String street, int section, int threshold) {
 		return this.getTownInstance().addSensor(sid, street, section, threshold);
 	}
 
+	// function to delete the sensor from the database
 	@Override
 	public int deleteSensor(String sid) {
 		return this.getTownInstance().deleteSensor(sid);
 	}
 
+	// function to update the strain value stored in the sensor identified by `sid`
 	@Override
 	public boolean updateStrain(String sid, int newStrain) {
 		return this.getTownInstance().updateStrain(sid, newStrain);
 	}
 
+	// function to reset the strain values in the sensor
 	@Override
 	public boolean resetSensor(String sid) {
 		return this.getTownInstance().resetSensor(sid);
 	}
 
+	// function to get all the sensors in the database
 	@Override
 	public List<Sensor> getAllSensors() {
 		return this.getTownInstance().getAllSensors();
 	}
 
+	// function to search for a specific sensor, identified by a street name
+	// and section number
 	@Override
 	public Sensor searchForSensor(String street, int section) {
 		return this.getTownInstance().searchForSensor(street, section);
 	}
 
+	// function to get the sensors that are in critical condition
 	@Override
 	public List<Sensor> getCriticalSensors() {
 		return this.getTownInstance().getCriticalSensors();
 	}
 
+	// function to get the sensors that are considered dead
 	@Override
 	public List<Sensor> getDeadSensors() {
 		return this.getTownInstance().getDeadSensors();
 	}
 
+	// function to create a service request to repair section(s) of a street
+	// or bridge
 	@Override
 	public boolean createServiceRequest(String srid, String[] sid_arr) {
 		return this.getTownInstance().createServiceRequest(srid, sid_arr);
 	}
 	
+	// function to search for a specific sensor, identified by the sensor
+	// id number
 	@Override
 	public Sensor searchForSensor(String sid) {
 		return this.getTownInstance().searchForSensor(sid);
 	}
 	
+	// function to create a singleton of `town`
 	private Town getTownInstance(){
 		if(town == null){
 			return new Town();
